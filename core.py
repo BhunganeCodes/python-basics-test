bd81d086a5e0f1a83d71a03e99aded62660c9d6b
BhunganeCodes <thamsanqahadebe98@gmail.com>
2026-02-09 10:33:52 +0200

    solved: is triangle numbers

--- core.py
+++ core.py
@@ -1,14 +1,21 @@
 
 
 def repeat_message(msg, n):
-    
+
     for i in range(n):
         print(msg)
     
 
 def is_triangle_number(n):
-    pass
+    res = 0
 
+    for i in range(1, n+1):
+        res += i
+    
+    if res % 3 == 0:
+        return True
+    else:
+        return False
 
 def print_odds_down(n):
     pass
